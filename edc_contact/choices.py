61c880f583b4113dfda141e785d9fe930f0f9bb2
erik van widenfelt <ew2789@gmail.com>
2015-05-22 09:50:54 +0200

    merge

--- edc_contact/choices.py
+++ edc_contact/choices.py
@@ -1,7 +1,27 @@
+from django.utils.translation import ugettext as _
 
 FOLLOW_UP = (
     ('sms', 'SMS'),
     ('call', 'Call'),
     ('in_person', 'In Person'),
     ('mail', 'mail'),
+)
+
+APPT_LOCATIONS = (
+    ('home', 'At home'),
+    ('work', 'At work'),
+    ('clinic', 'At clinic'),
+    ('OTHER', 'Other location'),
+)
+
+APPT_GRADING = (
+    ('firm', 'Firm appointment'),
+    ('weak', 'Possible appointment'),
+    ('guess', 'Estimated by RA'),
+)
+
+CONTACT_TYPE = (
+    ('direct', 'Direct edc_contact with participant'),
+    ('indirect', 'Contact with person other than participant'),
+    ('no_contact', 'No edc_contact made'),
 )
